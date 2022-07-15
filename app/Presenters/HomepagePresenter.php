beb5411f2ef748f8bd1617bcd25b96cb003145ce
Matěj <matej@appsdevteam.com>
2022-07-15 09:56:51 +0200

    úprava inject na starší verzi nette, oopravena verze php

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
-use Nette\DI\Attributes\Inject;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
 
-	#[Inject]
+	/** @var Nette\Database\Explorer @inject */
 	public Nette\Database\Explorer $db;
 
 	public function actionDefault()
